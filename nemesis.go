a3fa2544085da05820919fdc352f8a3aea2f8c3d
Gui Lin <guileen@gmail.com>
2015-12-23 16:22:55 +0800

    add cli arguments

--- nemesis.go
+++ nemesis.go
@@ -6,29 +6,81 @@ import (
 	"path/filepath"
 	"time"
 
-	"git.coding.net/leeen/thel_reverse_proxy/modules"
+	"github.com/codegangsta/cli"
+	"github.com/guileen/nemesis/modules"
 )
 
 func main() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal("Err to get working dir", err)
-	}
-	log.Println("dir", dir)
 
-	freader, err := os.Open(dir + "/conf/config.yaml")
-	if err != nil {
-		log.Fatal("Error to load", err)
+	app := cli.NewApp()
+	app.Name = "nemesis"
+	app.Usage = "the server"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Value: "",
+		},
 	}
-	node, err := modules.Parse(freader)
-	if err != nil {
-		log.Fatal("Error to parse", err)
+
+	log.SetFlags(log.Ltime | log.Lshortfile)
+
+	app.Action = func(c *cli.Context) {
+
+		configFile := c.String("config")
+		log.Println("configFile", configFile)
+		if configFile == "" {
+			cwd, err := os.Getwd()
+			if err != nil {
+				log.Fatal("Error to get current working directory", err)
+			}
+			// run as static server in current directory
+			rootConfig := modules.Map{
+				"*": modules.Map{
+					"default": modules.Scalar("true"),
+					"port":    modules.Scalar("8080"),
+					"routes": modules.List{
+						modules.Map{
+							"*": modules.Map{
+								"static": modules.Scalar(cwd),
+							},
+						},
+					},
+				},
+			}
+			root := modules.NewRootModule(rootConfig)
+			root.Run()
+			for {
+				<-time.After(10 * time.Second)
+			}
+			return
+		}
+		configFile, err := filepath.Abs(configFile)
+		if err != nil {
+			log.Fatal("Error to get config file", err)
+		}
+
+		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			log.Fatal("Err to get working dir", err)
+		}
+		log.Println("dir", dir)
+
+		freader, err := os.Open(configFile)
+		if err != nil {
+			log.Fatal("Error to load", err)
+		}
+		node, err := modules.Parse(freader)
+		if err != nil {
+			log.Fatal("Error to parse", err)
+		}
+		// 	log.Println(node)
+		rootModule := modules.NewRootModule(node)
+		log.Println("RootModule", rootModule)
+		rootModule.Run()
+		for {
+			<-time.After(10 * time.Second)
+		}
 	}
-	// 	log.Println(node)
-	rootModule := modules.NewRootModule(node)
-	log.Println("RootModule", rootModule)
-	rootModule.Run()
-	for {
-		<-time.After(10 * time.Second)
-	}
+
+	app.Run(os.Args)
 }
